17428878463ceb29b5d242fcb751d0e25f81b6d9
ahmed mostafa <ahmed996mostafa@gmail.com>
2024-03-15 00:47:19 +0200

    solve conflict

--- ExSys/Forms/topFormControl.Designer.cs
+++ ExSys/Forms/topFormControl.Designer.cs
@@ -28,47 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            X_pictureBox = new PictureBox();
             label1 = new Label();
-            ((System.ComponentModel.ISupportInitialize)X_pictureBox).BeginInit();
             SuspendLayout();
-            // 
-            // X_pictureBox
-            // 
-            X_pictureBox.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            X_pictureBox.Location = new Point(497, 7);
-            X_pictureBox.Margin = new Padding(0);
-            X_pictureBox.Name = "X_pictureBox";
-            X_pictureBox.Size = new Size(100, 50);
-            X_pictureBox.TabIndex = 0;
-            X_pictureBox.TabStop = false;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(72, 12);
+            label1.Location = new Point(82, 16);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(50, 20);
             label1.TabIndex = 1;
             label1.Text = "label1";
             // 
             // topFormControl
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(label1);
-            Controls.Add(X_pictureBox);
             Margin = new Padding(0);
             Name = "topFormControl";
-            Size = new Size(605, 65);
-            ((System.ComponentModel.ISupportInitialize)X_pictureBox).EndInit();
+            Size = new Size(691, 87);
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private PictureBox X_pictureBox;
         private Label label1;
     }
 }
