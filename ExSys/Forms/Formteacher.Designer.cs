3f0d8f613ecdad83ba2e0bd0cf8feaa95165ba67
ahmed mostafa <ahmed996mostafa@gmail.com>
2024-03-14 22:42:21 +0200

    add first design for teacher form

--- ExSys/Forms/Formteacher.Designer.cs
+++ ExSys/Forms/Formteacher.Designer.cs
@@ -28,12 +28,220 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Formteacher";
+            Profile = new TabControl();
+            tabPage1 = new TabPage();
+            label1 = new Label();
+            tabPage2 = new TabPage();
+            label2 = new Label();
+            tabPage3 = new TabPage();
+            tabControl1 = new TabControl();
+            tabPage5 = new TabPage();
+            label3 = new Label();
+            tabPage6 = new TabPage();
+            button1 = new Button();
+            tabPage8 = new TabPage();
+            tabPage4 = new TabPage();
+            tabPage7 = new TabPage();
+            Profile.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            tabPage3.SuspendLayout();
+            tabControl1.SuspendLayout();
+            tabPage5.SuspendLayout();
+            tabPage6.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Profile
+            // 
+            Profile.Controls.Add(tabPage1);
+            Profile.Controls.Add(tabPage2);
+            Profile.Controls.Add(tabPage3);
+            Profile.Controls.Add(tabPage4);
+            Profile.Controls.Add(tabPage7);
+            Profile.Dock = DockStyle.Fill;
+            Profile.Location = new Point(0, 0);
+            Profile.Name = "Profile";
+            Profile.SelectedIndex = 0;
+            Profile.Size = new Size(800, 450);
+            Profile.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(label1);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(792, 417);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Profile";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(98, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(label2);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(732, 353);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Mycourses";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(263, 137);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 0;
+            label2.Text = "label2";
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(tabControl1);
+            tabPage3.Location = new Point(4, 29);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(732, 353);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Question";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage5);
+            tabControl1.Controls.Add(tabPage6);
+            tabControl1.Controls.Add(tabPage8);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(3, 3);
+            tabControl1.Margin = new Padding(0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.Padding = new Point(0, 0);
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(726, 347);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage5
+            // 
+            tabPage5.Controls.Add(label3);
+            tabPage5.Location = new Point(4, 29);
+            tabPage5.Name = "tabPage5";
+            tabPage5.Padding = new Padding(3);
+            tabPage5.Size = new Size(718, 314);
+            tabPage5.TabIndex = 0;
+            tabPage5.Text = "Add Question";
+            tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(311, 91);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 0;
+            label3.Text = "label3";
+            // 
+            // tabPage6
+            // 
+            tabPage6.Controls.Add(button1);
+            tabPage6.Location = new Point(4, 29);
+            tabPage6.Margin = new Padding(0);
+            tabPage6.Name = "tabPage6";
+            tabPage6.Padding = new Padding(3);
+            tabPage6.Size = new Size(718, 314);
+            tabPage6.TabIndex = 1;
+            tabPage6.Text = "Update Question";
+            tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(396, 87);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage8
+            // 
+            tabPage8.Location = new Point(4, 29);
+            tabPage8.Name = "tabPage8";
+            tabPage8.Padding = new Padding(3);
+            tabPage8.Size = new Size(718, 314);
+            tabPage8.TabIndex = 2;
+            tabPage8.Text = "ViewQuestion";
+            tabPage8.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            tabPage4.Location = new Point(4, 29);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Padding = new Padding(3);
+            tabPage4.Size = new Size(732, 353);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "Reports";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage7
+            // 
+            tabPage7.Location = new Point(4, 29);
+            tabPage7.Name = "tabPage7";
+            tabPage7.Padding = new Padding(3);
+            tabPage7.Size = new Size(732, 353);
+            tabPage7.TabIndex = 4;
+            tabPage7.Text = "Exam";
+            tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // Formteacher
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Profile);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Formteacher";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Formteacher";
+            WindowState = FormWindowState.Maximized;
+            Profile.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            tabPage3.ResumeLayout(false);
+            tabControl1.ResumeLayout(false);
+            tabPage5.ResumeLayout(false);
+            tabPage5.PerformLayout();
+            tabPage6.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl Profile;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private Label label1;
+        private TabControl tabControl1;
+        private TabPage tabPage5;
+        private TabPage tabPage6;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private TabPage tabPage7;
+        private TabPage tabPage8;
     }
 }
--- ExSys/Forms/Formteacher.cs
+++ ExSys/Forms/Formteacher.cs
@@ -16,5 +16,7 @@ namespace ExSys.Forms
         {
             InitializeComponent();
         }
+
+       
     }
 }
