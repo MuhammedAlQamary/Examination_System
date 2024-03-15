447cd1d3970802e8972d8ed10bb94c0c3632d65b
EmadAhmedNasef <154742346+EmadAhmedNasef@users.noreply.github.com>
2024-03-15 15:59:56 +0200

    Basic UI for Login

--- ExSys/Forms/loginForm.Designer.cs
+++ ExSys/Forms/loginForm.Designer.cs
@@ -28,12 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginForm));
+            label1 = new Label();
+            label2 = new Label();
+            txtEmail = new TextBox();
+            txtPass = new TextBox();
+            label3 = new Label();
+            BtnClose = new PictureBox();
+            BtnMinus = new PictureBox();
+            Login = new Button();
+            lblError = new Label();
+            chkBoxPass = new CheckBox();
+            ((System.ComponentModel.ISupportInitialize)BtnClose).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)BtnMinus).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.ForeColor = Color.FromArgb(0, 117, 224);
+            label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(label2, "label2");
+            label2.ForeColor = Color.FromArgb(0, 117, 224);
+            label2.Name = "label2";
+            // 
+            // txtEmail
+            // 
+            resources.ApplyResources(txtEmail, "txtEmail");
+            txtEmail.Name = "txtEmail";
+            // 
+            // txtPass
+            // 
+            resources.ApplyResources(txtPass, "txtPass");
+            txtPass.Name = "txtPass";
+            txtPass.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(label3, "label3");
+            label3.ForeColor = Color.FromArgb(0, 117, 224);
+            label3.Name = "label3";
+            // 
+            // BtnClose
+            // 
+            BtnClose.Cursor = Cursors.Hand;
+            resources.ApplyResources(BtnClose, "BtnClose");
+            BtnClose.Name = "BtnClose";
+            BtnClose.TabStop = false;
+            BtnClose.Click += BtnClose_Click;
+            // 
+            // BtnMinus
+            // 
+            BtnMinus.Cursor = Cursors.Hand;
+            resources.ApplyResources(BtnMinus, "BtnMinus");
+            BtnMinus.Name = "BtnMinus";
+            BtnMinus.TabStop = false;
+            BtnMinus.Click += BtnMinus_Click;
+            // 
+            // Login
+            // 
+            Login.Cursor = Cursors.Hand;
+            resources.ApplyResources(Login, "Login");
+            Login.ForeColor = Color.FromArgb(0, 117, 224);
+            Login.Name = "Login";
+            Login.UseVisualStyleBackColor = true;
+            Login.Click += Login_Click;
+            // 
+            // lblError
+            // 
+            resources.ApplyResources(lblError, "lblError");
+            lblError.ForeColor = Color.Red;
+            lblError.Name = "lblError";
+            // 
+            // chkBoxPass
+            // 
+            resources.ApplyResources(chkBoxPass, "chkBoxPass");
+            chkBoxPass.Name = "chkBoxPass";
+            chkBoxPass.UseVisualStyleBackColor = true;
+            chkBoxPass.CheckedChanged += chkBoxPass_CheckedChanged;
+            // 
+            // loginForm
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            Controls.Add(chkBoxPass);
+            Controls.Add(lblError);
+            Controls.Add(Login);
+            Controls.Add(BtnMinus);
+            Controls.Add(BtnClose);
+            Controls.Add(txtPass);
+            Controls.Add(label3);
+            Controls.Add(txtEmail);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "loginForm";
+            WindowState = FormWindowState.Maximized;
+            Load += loginForm_Load;
+            ((System.ComponentModel.ISupportInitialize)BtnClose).EndInit();
+            ((System.ComponentModel.ISupportInitialize)BtnMinus).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtEmail;
+        private TextBox txtPass;
+        private Label label3;
+        private PictureBox BtnClose;
+        private PictureBox BtnMinus;
+        private Button Login;
+        private Label lblError;
+        private CheckBox chkBoxPass;
     }
 }
--- ExSys/Forms/loginForm.cs
+++ ExSys/Forms/loginForm.cs
@@ -1,10 +1,106 @@
+using ExSys.Forms;
+using Microsoft.VisualBasic.ApplicationServices;
+
 namespace ExSys
 {
     public partial class loginForm : Form
     {
+        /*
+        private readonly Dictionary<string, string> teachers = new Dictionary<string, string>
+        {
+            { "teacher1@teacher.com", "teacher1" },
+            { "teacher2@teacher.com", "teacher2" },
+            
+        };
+        private readonly Dictionary<string, string> students = new Dictionary<string, string>
+        {
+            { "student1@student.com", "student1" },
+            { "student2@student.com", "student2" },
+           
+        };
+        */
+
         public loginForm()
         {
             InitializeComponent();
+        }
+
+        private void loginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnMinus_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text.Trim();
+            string password = txtPass.Text;
+
+            if (email == "admin@admin.com" && password == "admin12345")
+            {
+                OpenAdminForm();
+
+            }
+            else if (email == "teacher@teacher.com" && password == "teacher12345")
+            {
+                OpenTeacherForm();
+            }
+            /*
+            else if (teachers.ContainsKey(email) && teachers[email] == password)
+            {
+                OpenTeacherForm();
+            }
+            */
+            else
+            {
+                ShowErrorMessage("Invalid email or password.");
+            }
+
+
+        }
+
+
+
+        private void OpenAdminForm()
+        {
+
+            adminForm adminForm = new adminForm();
+            adminForm.Show();
+            this.Hide();
+        }
+
+        private void OpenTeacherForm()
+        {
+            Formteacher teacherForm = new Formteacher();
+            teacherForm.Show();
+            this.Hide();
+        }
+
+        private void ShowErrorMessage(string message)
+        {
+            lblError.Text = message;
+            lblError.Visible = true;
+        }
+
+        private void chkBoxPass_CheckedChanged(object sender, EventArgs e)
+        {
+            if(chkBoxPass.Checked)
+            {
+                txtPass.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtPass.UseSystemPasswordChar = true;
+            }
         }
     }
 }
